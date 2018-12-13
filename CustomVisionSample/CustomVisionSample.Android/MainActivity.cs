a1d78d0ea4651faca1862ca420c9f44e872d2059
tonkun-no <tonkun_no@ae.auone-net.jp>
2018-12-14 03:46:48 +0900

    プロジェクト ファイルを追加します。

---
+++ CustomVisionSample/CustomVisionSample.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.CurrentActivity;
+
+[assembly: UsesFeature("android.hardware.camera", Required = false)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
+namespace CustomVisionSample.Droid
+{
+    [Activity(Label = "CustomVisionSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        // Xam.Plugin.Media用に追加
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ CustomVisionSample/CustomVisionSample/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+
+namespace CustomVisionSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private string ProjectID = "ProjectID";
+        private string PredictionKey = "PredictionKey";
+        private string IterationID = "IterationID";
+        private string Endpoint = @"https://southcentralus.api.cognitive.microsoft.com";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            // 写真撮影
+            // Cameraを起動して写真を撮影（Plugin任せ）
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                DefaultCamera = CameraDevice.Front,
+                AllowCropping = false,
+                PhotoSize = PhotoSize.Medium,
+            });
+
+            // 撮影しなかった場合は何もしない
+            if (file == null) return;
+
+            // 画像を表示
+            image.Source = ImageSource.FromStream(() => file.GetStream());
+
+            try
+            {
+                // CustomVision用クライアント準備
+                // ApiKeyにはPredictionKeyを指定
+                // Endpointにはhttps://southcentralus.api.cognitive.microsoft.comを指定
+                var client = new CustomVisionPredictionClient()
+                {
+                    ApiKey = PredictionKey,
+                    Endpoint = Endpoint
+                };
+
+                // 画像を解析(IterationIDを指定しないとうまくいかない)
+                var resul = await client.PredictImageAsync(Guid.Parse(ProjectID), file.GetStream(), iterationId: Guid.Parse(IterationID));
+                // 結果取得
+                var prediction = resul.Predictions.FirstOrDefault();
+
+                await DisplayAlert("判定結果", $"タグ名: {prediction.TagName} Probability: {prediction.Probability}", "OK");
+            }
+            catch
+            {
+                await DisplayAlert("判定結果", $"エラー", "OK");
+            }
+
+        }
+    }
+}
